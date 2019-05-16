2f98dccef3f4dd582d3fa0b3c45c4d4e85c5e7a4
voluble-bug-0219 <montensia1@outlook.com>
2019-05-16 03:13:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,14 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarfs_array)
   # Your code here
+  i = 0 
+  while i < dwarfs_array.length 
+    yield(dwarfs_array[i])
+    i = i + 1 
+  end
+end
+dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
+roll_call_dwarves(dwarfs) do |i|
+  puts "#{i + 1}.to_str #{dwarfs[i]}"
 end
 
 def summon_captain_planet# code an argument here
